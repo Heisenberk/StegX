0cb696293ff093cd8013261c97b3451963bcd752
Pierre Ayoub <drac78@live.fr>
2018-02-26 23:42:59 +0100

    CMake : Première mise en place
    - Structure basique du projet.
    - Tests avec deux exécutables : CLI et GUI.
    - Prochaine mise en place : la bibliothèque partagée.
    - Génération des tags du projet générique entre les cibles.

---
+++ src/cli/maths/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "test.h"
+
+void math1 () {
+    printf("Math %d.\n", NUMBER);
+}
---
+++ src/cli/maths/test.h
@@ -0,0 +1,8 @@
+#ifndef TEST_H
+#define TEST_H
+
+#define NUMBER 1
+void math1();
+
+
+#endif
