57768ec9738cc8a155d5d8f2e653ad27335236dc
Pierre Ayoub <drac78@live.fr>
2018-03-05 22:46:43 +0100

    CPack : cible perso de package
    Problème : lors de la création des packages, CMake installe le projet
    temporairement puis le désinstalle. Pourtant, le raccourci lanceur ainsi que
    son image ne sont pas correctement supprimés.
    
    Tentative de solution après de moultes tentatives :
    - Création de deux modes de désinstallation : verbeux et silencieux.
    - Création de la cible personnalisée "all-package" pour créer les packages puis
      lancer la désinstallation (partiel) silencieuse.
    
    -----------
    
    Divers :
    - Utilisation de variable pour les chemins de l'icône.

--- config/cmake/uninstall/uninstall-target.cmake
+++ config/cmake/uninstall/uninstall-target.cmake
@@ -1,9 +1,20 @@
-# Cible de désinstallation.
 if (NOT TARGET uninstall)
+    # Cible de désinstallation silencieuse.
+    set (UNINSTALL_VERBOSE false)
+    configure_file (
+        "${CMAKE_SOURCE_DIR}/${CONFIG_PATH}/uninstall/uninstall-command.cmake.in"
+        "${CMAKE_BINARY_DIR}/cmake_uninstall_silent.cmake"
+        @ONLY
+        )
+    add_custom_target (package-cleanup
+        COMMAND ${CMAKE_COMMAND} -P ${CMAKE_BINARY_DIR}/cmake_uninstall_silent.cmake
+        )
+    # Cible de désinstallation verbeuse.
+    set (UNINSTALL_VERBOSE true)
     configure_file (
         "${CMAKE_SOURCE_DIR}/${CONFIG_PATH}/uninstall/uninstall-command.cmake.in"
         "${CMAKE_BINARY_DIR}/cmake_uninstall.cmake"
-        IMMEDIATE @ONLY
+        @ONLY
         )
     add_custom_target (uninstall
         COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --cyan "Uninstalling the project..."
