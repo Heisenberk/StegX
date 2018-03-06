734ee23c1b8d4c58216b93de1668deb4394486fb
Pierre Ayoub <drac78@live.fr>
2018-03-06 20:18:22 +0100

    CMake : améliorations cibles uninstall & package
    - Script de désinstallation optimisé et adapté en fonction de l'OS.
    - Nouvelle méthode de désinstallation propre pour les UNIX, qui permet
    d'effacer toutes traces de StegX. La méthode multiplate-forme ne
    supprime que les fichiers installés, mais pas les dossiers.
    - Ajout des 3 modules principaux en tant que dépendances de la création
    des packages.
    - La cible de création des packages bénéficie de la cible de
    désinstallation propre à UNIX.

--- config/cmake/uninstall/uninstall-target.cmake
+++ config/cmake/uninstall/uninstall-target.cmake
@@ -1,23 +1,44 @@
 if (NOT TARGET uninstall)
-    # Cible de désinstallation silencieuse.
-    set (UNINSTALL_VERBOSE false)
-    configure_file (
-        "${CMAKE_SOURCE_DIR}/${CONFIG_PATH}/uninstall/uninstall-command.cmake.in"
-        "${CMAKE_BINARY_DIR}/cmake_uninstall_silent.cmake"
-        @ONLY
-        )
-    add_custom_target (package-cleanup
-        COMMAND ${CMAKE_COMMAND} -P ${CMAKE_BINARY_DIR}/cmake_uninstall_silent.cmake
-        )
-    # Cible de désinstallation verbeuse.
+    # Variables.
+    set (UNINSTALL_CMAKE_SCRIPT
+        "${CMAKE_SOURCE_DIR}/${CONFIG_PATH}/uninstall/uninstall-command.cmake.in")
+    set (UNINSTALL_SH_SCRIPT
+        "${CMAKE_SOURCE_DIR}/${CONFIG_PATH}/uninstall/uninstall-command.sh.in")
+    set (UNINSTALL_SILENT_OUTPUT "${CMAKE_BINARY_DIR}/cmake_uninstall_silent.cmake")
+    set (UNINSTALL_VERBOSE_OUTPUT "${CMAKE_BINARY_DIR}/cmake_uninstall.cmake")
+    set (UNINSTALL_SH_OUTPUT "${CMAKE_BINARY_DIR}/cmake_uninstall.sh")
+    set (UNINSTALL_TARGET_1 uninstall)
+    set (UNINSTALL_TARGET_3 package-cleanup)
+
+    # Cas spécial pour les UNIX, voir la cible plus bas.
+    if (UNIX)
+        set (UNINSTALL_TARGET_1 uninstall-files)
+        set (UNINSTALL_TARGET_2 uninstall)
+    endif()
+
+    # Cible de désinstallation verbeuse multiplate-forme (cible 1). Ne
+    # désinstalle que les fichiers installés, mais pas les dossiers.
     set (UNINSTALL_VERBOSE true)
-    configure_file (
-        "${CMAKE_SOURCE_DIR}/${CONFIG_PATH}/uninstall/uninstall-command.cmake.in"
-        "${CMAKE_BINARY_DIR}/cmake_uninstall.cmake"
-        @ONLY
-        )
-    add_custom_target (uninstall
+    configure_file ( ${UNINSTALL_CMAKE_SCRIPT} ${UNINSTALL_VERBOSE_OUTPUT} @ONLY)
+    add_custom_target (${UNINSTALL_TARGET_1}
         COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --cyan "Uninstalling the project..."
-        COMMAND ${CMAKE_COMMAND} -P ${CMAKE_BINARY_DIR}/cmake_uninstall.cmake
+        COMMAND ${CMAKE_COMMAND} -P ${UNINSTALL_VERBOSE_OUTPUT}
         )
+
+    # Cible de désinstallation silencieuse uniquement pour UNIX (cible 2).
+    # Désinstalle les fichiers et les dossiers installés.
+    if (UNIX)
+        configure_file (${UNINSTALL_SH_SCRIPT} ${UNINSTALL_SH_OUTPUT} @ONLY)
+        add_custom_target (${UNINSTALL_TARGET_2} COMMAND ${UNINSTALL_SH_OUTPUT})
+        add_custom_target (${UNINSTALL_TARGET_3} COMMAND ${UNINSTALL_SH_OUTPUT})
+        add_dependencies (${UNINSTALL_TARGET_2} ${UNINSTALL_TARGET_1})
+    else ()
+        # Cible de désinstallation silencieuse multiplate-forme (cible 3). Ne
+        # désinstalle que les fichiers installés, mais pas les dossiers. Utile
+        # pour la désinstallation des fichiers résidus de la création des
+        # packages. Pour UNIX, cette fonction est déjà assuré par la cible 2.
+        set (UNINSTALL_VERBOSE false)
+        configure_file ( ${UNINSTALL_CMAKE_SCRIPT} ${UNINSTALL_SILENT_OUTPUT} @ONLY)
+        add_custom_target (${UNINSTALL_TARGET_3} COMMAND ${CMAKE_COMMAND} -P ${UNINSTALL_SILENT_OUTPUT})
+    endif()
 endif()
