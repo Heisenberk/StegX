8b570c9c1a2ce9ed0d5ae3ade800dd06a992a7c9
Tristan <tristan.bessac@live.fr>
2018-03-27 18:53:10 +0200

    adaptation du code aux nouvelles structures

--- src/cli/commandes/cli.c
+++ src/cli/commandes/cli.c
@@ -19,7 +19,7 @@ void we_are_stegx(){
   printf("Faire \033[01m\033[31mstegx --help\033[0m ou \033[01m\033[31mstegx -h \033[0m pour un rappel des commandes.\n\n\n");
 }
 
-void besoin_d_aide(){
+void help(){
     printf("Manuel d'utilisation de stegx\n\n");
     printf("\033[01m\033[32m-d, --dissimule \033[0m\n\t Option de dissimultation.\n\n"
 		    "\033[01m\033[32m-e, --extraire \033[0m\n\t Option d'extraction.\n\n"
@@ -36,33 +36,34 @@ void besoin_d_aide(){
    printf("\n"); 
 }
 
-void ligne_non_valide(){
+void unvalid_line(){
   //quelque chose à afficher +
-  besoin_d_aide();
+  help();
 }
 
-void remplir_info(struct info* com,const int argc,char* const* argv){
+
+void remplir_info( stegx_info_t* com,const int argc,char* const* argv){
     int optch;
     extern int opterr;
 
-    char option_court[] = "o:c:r:p:deh"; // correspond au format taper en ligne de commande,  x: signifie qu'un argument est attendu après -x  
+    char short_option[] = "o:c:r:p:deh"; // correspond au format taper en ligne de commande,  x: signifie qu'un argument est attendu après -x  
                   //o: pour l'option -o suivi du fichier hote
                   //c: pour l'option -c suivi du fichier à cacher 
                   //r: pour l'option -r suivi du fichier resultat
                   //p: pour l'option -p suivi du mot de passe 
-                  	//a: pour l'option -a suivi de l'algo à utiliser   !!! à voir avec groupe
+                    //a: pour l'option -a suivi de l'algo à utiliser   !!! à voir avec groupe
                   //d pour l'option -d pour signifier la dissimulation
                   //e pour l'option -e pour signifier l'extraction
                   //h pour l'option -h qui affiche l'aide
-     const struct option option_long[] = {
+     const struct option long_option[] = {
         {"hote", 1, NULL, 'o'},
         {"cache", 1, NULL, 'c'},
         {"resultat", 1, NULL, 'r'},
-	{"dissimule", 0, NULL, 'd'},
-	{"extraire", 0, NULL, 'e'}, 
-        {"lsb",0, NULL,ALGO_LSB},
-        {"metadata",0,NULL,ALGO_METADATA},
-        {"EOF",0,NULL, ALGO_EOF},
+  {"dissimule", 0, NULL, 'd'},
+  {"extraire", 0, NULL, 'e'}, 
+        {"lsb",0, NULL,STEGX_ALGO_LSB},
+        {"metadata",0,NULL,STEGX_ALGO_METADATA},
+        {"EOF",0,NULL, STEGX_ALGO_EOF},
         {"password", 1, NULL, 'p'},
         {"help",0,NULL,'h'},
         {NULL, 0, NULL, 0}
@@ -73,65 +74,50 @@ void remplir_info(struct info* com,const int argc,char* const* argv){
     while ((optch = getopt_long(argc, argv, option_court, option_long, &optindex)) != -1)
     switch (optch) {
         case 'o':
-            com->hote=optarg;
+            com->host_path=optarg;
             break;
         case 'c':
-            com->cache=optarg;
+            com->ins_info->hidden_path=optarg;
             break; 
         case 'r':
-            com->resultat=optarg;
+            com->res_path=optarg;
             break;
        
         case 'd':
-            com->d_e=1; 
+            com->mode=STEGX_MODE_INSERT; 
             break;
         case 'e':
-             com->d_e=0;
+             com->mode=STEGX_MODE_EXTRACT;
             break;
         case 'p':
-             com->mdp=optarg;
-        case ALGO_LSB:
-             com->algo=ALGO_LSB;
+             com->passwd=optarg;
+        case STEGX_ALGO_LSB:
+             com->ins_info->algo=STEGX_ALGO_LSB;
              break;
-        case ALGO_METADATA:
-             com->algo=ALGO_METADATA;
+        case STEGX_ALGO_METADATA:
+             com->ins_info->algo=STEGX_ALGO_METADATA;
              break;           
-        case ALGO_EOF:
-             com->algo=ALGO_EOF;
+        case STEGX_ALGO_EOF:
+             com->ins_info->algo=STEGX_ALGO_EOF;
              break;
         case 'h':
-             com->d_e = 3;
-	     break;
+             help();
+             exit(0);
+              break;
 
 };
  
 }
 
-void verif_infos(struct info* com){
-  if (com->d_e==1){
-    if((com->hote!="\0") && (com->cache!="\0") && (com->resultat!="\0") && (com->algo!=-1)){
+void verif_infos(stegx_info_t* com){
+  if (com->mode==STEGX_MODE_INSERT){
+    if((com->ins_info->hidden_path!="\0") && (com->ins_info->algo!=-1)){
      // lancement_stegx(com);
      printf("c'est ok\n");
     }
-    else{
-      ligne_non_valide();
-    }  
-  }
-  else if (com->d_e==0){
-    if((com->hote!="\0") && (com->resultat!="\0")){
-        //lancement_stegx(com);
-
-     printf("c'est ok\n");
-    }
-    else{
-        ligne_non_valide();
-    }
+    else{unvalid_line();}
 
   }
-  else if(com->d_e == 3)
-	  besoin_d_aide();
-  else 
-	  ligne_non_valide();
-   
+    else {unvalid_line();}
 }
 
