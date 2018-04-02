b108cc14760b4e8a83443acefccc26cba3d4884f
Claire B <user@debian.arcahe.ovh>
2018-04-02 18:03:53 +0200

    modification de la cli
    - ajout de l'option --about
    - modification des sorties du programme : affiche maintenant
    les informations contenues dans les structures

--- src/cli/commandes/cli.c
+++ src/cli/commandes/cli.c
@@ -15,18 +15,12 @@
 #include <getopt.h>
 
 stegx_info_t* init_stegx_info(){
-    stegx_info_ins_t* com_ins = calloc(1,sizeof(stegx_info_ins_t));
     stegx_info_t* com = calloc(1,sizeof(stegx_info_t));
     com->host_path = "stdin\0";
     com->res_path = "stdout\0";
-    com->ins_info=com_ins;
+    com->ins_info=calloc(1, sizeof(stegx_info_ins_t));
     return com;
-
-
 }
-
-
-
 
 void we_are_stegx(){
   printf("   _____ _            __   __\n");
@@ -47,7 +41,8 @@ void we_are_stegx(){
 
 void help(){
     printf("Manuel d'utilisation de stegx\n\n");
-    printf("\033[01m\033[32m-d, --dissimule \033[0m\n\t Option de dissimultation.\n\n"
+    printf("\033[01m\033[32m-a, --about \033[0m\n\t A propos de l'application et de ses concepteurs\n\n"
+        "\033[01m\033[32m-d, --dissimule \033[0m\n\t Option de dissimultation.\n\n"
         "\033[01m\033[32m-e, --extraire \033[0m\n\t Option d'extraction.\n\n"
         "\033[01m\033[32m-o, --hote [nom_fichier]\033[0m\n\t Le nom du fichier hote.\n\n"
         "\033[01m\033[32m-c, --cache [nom_fichier]\033[0m (si option de dissimulation sélectionnée)"
@@ -55,7 +50,7 @@ void help(){
         "\033[01m\033[32m-r, --resultat [nom_fichier]\033[0m\n\t Le nom du fichier où stocker le résultat.\n\n"
         "\033[01m\033[32m-p, --password [password]\033[0m (optionnel)\n\t Pour ajouter un mot de passe permettant "
         "de rajouter une protection à la dissimulation.\n\n"
-        "Voici les différents algorithmes que propose l'application :\n"
+        "Voici les différents algorithmes que propose l'application (si option de dissimulation sélectionnée) :\n"
         "\033[01m\033[32m--lsb\033[0m : least significant bit. \n"
         "\033[01m\033[32m--metadata\033[0m \n"
         "\033[01m\033[32m--EOF\033[0m : end of file.\n");
@@ -69,17 +64,12 @@ void unvalid_line(){
   help();
 }
 
-
-
-
-
-
 void fill_info(stegx_info_t* com,const int argc,char* const* argv){
     if(argc==1){we_are_stegx();}
     int optch;
     extern int opterr;
 
-    char short_option[] = "o:c:r:p:deh"; // correspond au format taper en ligne de commande,  x: signifie qu'un argument est attendu après -x  
+    char short_option[] = "o:c:r:p:deha"; // correspond au format taper en ligne de commande,  x: signifie qu'un argument est attendu après -x  
                   //o: pour l'option -o suivi du fichier hote
                   //c: pour l'option -c suivi du fichier à cacher 
                   //r: pour l'option -r suivi du fichier resultat
@@ -98,16 +88,16 @@ void fill_info(stegx_info_t* com,const int argc,char* const* argv){
         {"EOF",0,NULL, STEGX_ALGO_EOF},
         {"password", 1, NULL, 'p'},
         {"help",0,NULL,'h'},
+	{"about",0,NULL,'a'},
         {NULL, 0, NULL, 0}
-};
+    };
     opterr = 1;
     int optindex = 0;
 
     while ((optch = getopt_long(argc, argv, short_option, long_option, &optindex)) != -1)
     switch (optch) {
-    case 'd':
-            com->mode=STEGX_MODE_INSERT;
-            
+        case 'd':
+            com->mode=STEGX_MODE_INSERT;   
             break;
         case 'e':
              com->mode=STEGX_MODE_EXTRACT;
@@ -135,28 +125,42 @@ void fill_info(stegx_info_t* com,const int argc,char* const* argv){
         case 'h':
              help();
              break;
+	case 'a':
+	     we_are_stegx();
+	     break;
         case '?':
-              unvalid_line();
-              break;
+             unvalid_line();
+             break;
     };
- 
 }
 
 void check_info(stegx_info_t* com){
   if (com->mode==STEGX_MODE_INSERT){
     if((com->ins_info->hidden_path!="\0") && (com->ins_info->algo!=-1)){
-      printf("stegx_check_compatibility_dissimulation(com);");
-     
+    //stegx_process(); 
+    printf("host_path : %s\n"
+            "hidden_path : %s\n"
+	    "res_path : %s\n"
+	    "algo : %d\n"
+	    "password : %s\n", com->host_path, com->ins_info->hidden_path,
+	    com->res_path, com->ins_info->algo, com->passwd);
     }
   }
   else if (com->mode==STEGX_MODE_EXTRACT){
-      printf("stegx_check_compatibility_extraction(com);");
+    //stegx_process(); 
+    printf("host_path : %s\n"
+	    "res_path : %s\n"
+	    "password : %s\n", com->host_path, com->res_path, com->passwd);
   }
-  else {unvalid_line();}
+  else {
+      unvalid_line();
   }
+}
 
 void dest_stegx_info(stegx_info_t* com){
-    if (com->ins_info){free(com->ins_info);}
+    if (com->ins_info){
+	    free(com->ins_info);
+    }
     free(com);
 
 }
