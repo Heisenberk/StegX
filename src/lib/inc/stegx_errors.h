17340c2b85d8c1bcb85ef8d074808cf48e5c16f3
Pierre Ayoub <drac78@live.fr>
2018-05-02 00:51:19 +0200

    Lib: base du module de gestion des erreurs.

---
+++ src/lib/inc/stegx_errors.h
@@ -0,0 +1,34 @@
+/** 
+ * @file stegx_errors.h
+ * @brief Module de gestion des erreurs.
+ * @details Contient les définitions des codes d'erreurs ainsi que la variable
+ * globale utilisée pour communiquer.
+ */
+
+#ifndef ERRORS_H
+#define ERRORS_H
+
+/**
+ * Liste des codes d'erreurs de la bibliothèque. Utilisé dans le retour des
+ * fonctions et dans la correspondance erreur <=> message. Le code 0 signifie
+ * (sauf cas particulier) qu'aucune erreur ne s'est produite.
+ */
+enum err_code {
+    ERR_NONE = 0, /*!< Aucune erreur. */
+    ERR_OTHER     /*!< Erreur quelconque. */
+};
+
+/**
+ * Variable mise à la disposition des fonctions de la bibliothèque pour y
+ * inscrire leur code d'erreur.
+ */
+enum err_code stegx_errno;
+
+/**
+ * Affiche le message d'erreur sur la sortie d'erreur en fonction du code
+ * d'erreur spécifié.
+ * @param err Code d'erreur.
+ */
+void err_print(const enum err_code err);
+
+#endif /* ERRORS_H */
