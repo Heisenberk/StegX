fbb61c608a64dd1ecc94c6cb68a16eee4fe79d0c
uvsq21501810 <clement.caumes@ens.uvsq.fr>
2018-05-02 18:41:17 +0200

    Division des tests unitaires

---
+++ test/lib/test_file_png.c
@@ -0,0 +1,71 @@
+/* Inclusions minimales. */
+#include <stdlib.h>
+#include <stdio.h>
+#include <setjmp.h>
+#include "cmocka.h"
+
+/* Inclusions supplémentaires. */
+#include <errno.h>
+#include <unistd.h>
+#include "stegx.h"
+#include "common.h"
+
+extern type_e check_file_format(FILE * file);
+
+void test_file_png_v1(void **state)
+{
+    (void)state;                /* Unused */
+    FILE *f = fopen("../../../env/test/inexistant", "r");
+    type_e test = stegx_test_file_png(f);
+    if (f != NULL)
+        fclose(f);
+    assert_int_equal(test, UNKNOWN);
+}
+
+void test_file_png_v2(void **state)
+{
+    (void)state;                /* Unused */
+    FILE *f = fopen("../../../env/test/inexistant", "r");
+    type_e test = stegx_test_file_png(f);
+    if (f != NULL)
+        fclose(f);
+    assert_int_equal(test, UNKNOWN);
+}
+
+void test_file_png_v3(void **state)
+{
+    (void)state;                /* Unused */
+    FILE *f = fopen("../../../env/test/test8.png", "r");
+    type_e test = stegx_test_file_png(f);
+    if (f != NULL)
+        fclose(f);
+    assert_int_equal(test, PNG);
+}
+
+void test_file_png_v4(void **state)
+{
+    (void)state;                /* Unused */
+    FILE *f = fopen("../../../env/test/test9.png", "r");
+    type_e test = stegx_test_file_png(f);
+    if (f != NULL)
+        fclose(f);
+    assert_int_equal(test, PNG);
+}
+
+/* Structure CMocka contenant la liste des tests. */
+const struct CMUnitTest check_compatibility_tests[] = {
+
+    // tests unitaires PNG
+    cmocka_unit_test(test_file_png_v1),
+    cmocka_unit_test(test_file_png_v2),
+    cmocka_unit_test(test_file_png_v3),
+    cmocka_unit_test(test_file_png_v4),
+
+};
+
+int main(void)
+{
+    /* Exécute les tests. */
+    return cmocka_run_group_tests(check_compatibility_tests, NULL, NULL);
+}
+
