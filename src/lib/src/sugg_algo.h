3b908102db57219951bc1df49f54a0e541749d9d
uvsq21501810 <clement.caumes@ens.uvsq.fr>
2018-05-02 13:45:26 +0200

    Ajout module suggest_algo

---
+++ src/lib/src/sugg_algo.h
@@ -0,0 +1,15 @@
+#ifndef SUGGALGO_H
+#define SUGGALGO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "stegx_common.h"
+#include "common.h"
+
+int fill_host_info(info_s* infos);
+
+int stegx_suggest_algo(info_s* infos);
+
+#endif
+
