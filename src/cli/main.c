5bad6e1e0beea8fe93db513b923a5de4710580ea
uvsq21501810 <clement.caumes@ens.uvsq.fr>
2018-05-09 12:34:26 +0200

    Correction claire

--- src/cli/main.c
+++ src/cli/main.c
@@ -9,8 +9,7 @@ int main(int argc, char *argv[])
     stegx_choices_s *com = init_stegx_info();
     fill_info(com, argc, argv);
     check_info(com);
-
-    info_s *infos = stegx_init(com);
+	info_s *infos = stegx_init(com);
     if (infos == NULL) {
         err_print(stegx_errno);
         return EXIT_FAILURE;
@@ -29,17 +28,18 @@ int main(int argc, char *argv[])
             return EXIT_FAILURE;
         }
         // a mettre com->insert_info->algo mettre STEGX_ALGO_EOF par defaut
-        int choosen = stegx_choose_algo(infos, STEGX_ALGO_EOF);
+        int choosen = stegx_choose_algo(infos,com->insert_info->algo);
         if (choosen == 1) {
+			stegx_errno=ERR_READ;
             err_print(stegx_errno);
             return EXIT_FAILURE;
         }
 
-        int insert = stegx_insert(infos);
+        /*int insert = stegx_insert(infos);
         if (insert == 1) {
             err_print(stegx_errno);
             return EXIT_FAILURE;
-        }
+        }*/
     }
     
     else if(com->mode==STEGX_MODE_EXTRACT){
