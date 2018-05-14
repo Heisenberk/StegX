518e6066a633dfe5c6213f6372e3714c73110ea6
uvsq21501810 <clement.caumes@ens.uvsq.fr>
2018-05-14 21:52:18 +0200

    Correction partiel

--- test/lib/file_type/test_png.c
+++ test/lib/file_type/test_png.c
@@ -11,6 +11,14 @@
 #include "stegx.h"
 #include "common.h"
 
+void dest_stegx_info(stegx_choices_s * com)
+{
+    if (com->insert_info) {
+        free(com->insert_info);
+    }
+    free(com);
+
+}
 
 void test_file_png_v1(void **state)
 {
@@ -111,13 +119,14 @@ void test_metadata_png_without_passwd(void **state)
 	choices_insert->host_path=malloc((strlen("../../../env/test/test9.png")+1)*sizeof(char));
 	strcpy(choices_insert->host_path,"../../../env/test/test9.png");
 	choices_insert->res_path=malloc((strlen("./short.txt")+1)*sizeof(char));
-	strcpy(choices_insert->res_path,"./res2_test_metadata.png");
-	choices_insert->passwd=NULL;
+	strcpy(choices_insert->res_path,"./res1_test_meta.png");
+	choices_insert->passwd=malloc((strlen("stegx")+1)*sizeof(char));
+	strcpy(choices_insert->passwd,"stegx");
 	choices_insert->mode=STEGX_MODE_INSERT;
 	choices_insert->insert_info=malloc(sizeof(stegx_info_insert_s));
 	choices_insert->insert_info->hidden_path=malloc((strlen("../../../env/test/short.txt")+1)*sizeof(char));
 	strcpy(choices_insert->insert_info->hidden_path,"../../../env/test/short.txt");
-	choices_insert->insert_info->algo=STEGX_ALGO_METADATA;
+	choices_insert->insert_info->algo=STEGX_ALGO_EOF;
 	int test;
 	
 	info_s *infos_insert = stegx_init(choices_insert);
@@ -137,11 +146,12 @@ void test_metadata_png_without_passwd(void **state)
 	stegx_clear(infos_insert);
 	
 	stegx_choices_s* choices_extract=malloc(sizeof(stegx_choices_s));
-	choices_extract->host_path=malloc((strlen("./res2_test_metadata.png")+1)*sizeof(char));
-	strcpy(choices_extract->host_path,"./res2_test_metadata.png");
+	choices_extract->host_path=malloc((strlen("./res1_test_meta.png")+1)*sizeof(char));
+	strcpy(choices_extract->host_path,"./res1_test_meta.png");
 	choices_extract->res_path=malloc((strlen("./")+1)*sizeof(char));
 	strcpy(choices_extract->res_path,"./");
-	choices_extract->passwd=NULL;
+	choices_extract->passwd=malloc((strlen("stegx")+1)*sizeof(char));
+	strcpy(choices_extract->passwd,"stegx");
 	choices_extract->mode=STEGX_MODE_EXTRACT;
 	choices_extract->insert_info=NULL;
 
@@ -157,6 +167,7 @@ void test_metadata_png_without_passwd(void **state)
 	assert_int_equal(test,0);
 	
 	stegx_clear(infos_extract);
+	dest_stegx_info(choices_extract);
 	
     uint8_t c;
     uint32_t i;
@@ -174,8 +185,7 @@ void test_metadata_png_without_passwd(void **state)
     free(message);
 	
     remove("./short.txt");
-	
-	
+    
 }
 
 /* Structure CMocka contenant la liste des tests. */
