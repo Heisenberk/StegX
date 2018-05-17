0020a56b6cdaad6f7b9f69fc37f6e1efe396c4ec
Claire <claire.baskevitch@ens.uvsq.fr>
2018-05-17 23:21:46 +0200

    dev-video : debuggage de insertion eoc

--- src/lib/src/algo/eoc.c
+++ src/lib/src/algo/eoc.c
@@ -42,7 +42,7 @@ int insert_eoc(info_s * infos)
     		if (fread(&byte_cpy, sizeof(uint8_t), 1, infos->host.host) != 1)
                 return perror("Can't read Header"), 1;
             if (fwrite(&byte_cpy, sizeof(uint8_t), 1, infos->res) == 0)
-                return perror("Can't writ Header"), 1;
+                return perror("Can't write Header"), 1;
             
     	}
     	
@@ -54,36 +54,36 @@ int insert_eoc(info_s * infos)
     		    	fwrite(&tag_type, sizeof(uint8_t), 1, infos->res);
     		    	fread(&data_size, sizeof(data_size), 1, infos->host.host);
     		    	fwrite(&data_size, sizeof(uint32_t), 1, infos->res);
-    		    	data_size = be32toh(data_size);
-            		//passage en 24 bits      
+    		    	//passage en 24 bits      
             	  	data_size >>= 8;
+    		    	data_size = be32toh(data_size);
         			//recopie data + 6 octets
         			for(uint32_t j=0;j<data_size+6;j++){
         				fread(&byte_cpy, sizeof(uint8_t), 1, infos->host.host);
     			  		fwrite(&byte_cpy, sizeof(uint8_t), 1, infos->res);
-    		      		
-            	 
     				}
     				//recopie prev tag size
     				fread(&prev_tag_size, sizeof(uint32_t), 1, infos->host.host);
     				fwrite(&prev_tag_size, sizeof(uint32_t), 1, infos->res);
-    		      	
-
     		}
     		else {
     			fwrite(&tag_type, sizeof(uint8_t), 1, infos->res);
     			fread(&data_size, sizeof(data_size), 1, infos->host.host);
     			 
     			/* /!\ */
-    			uint8_t tmp = data_size;
-
-    			data_size = be32toh(data_size);
-            	//passage en 24 bits      
+    			uint8_t tmp = data_size; //l'octet de droite qui fait parti des datas
+    			//passage en 24 bits      
             	data_size >>= 8;
+    			data_size = be32toh(data_size);
+    			
             	uint32_t data_size_host=data_size;
             	// /!\ verifier que la taille à inscrire < 2^24
-            	if (cpt_video_tag==infos->host.file_info.flv.nb_video_tag-1){data_size+=reste+data_per_vtag;} //cas particulier dernier tag
-            	else {data_size+=reste+data_per_vtag;}
+            	if (cpt_video_tag==infos->host.file_info.flv.nb_video_tag-1){
+					data_size+=reste+data_per_vtag;
+				} //cas particulier dernier tag
+            	else {
+					data_size+=reste+data_per_vtag;
+				}
             	
             	
             	uint8_t tmp2=data_size;
@@ -104,8 +104,12 @@ int insert_eoc(info_s * infos)
     			uint8_t nb_chunk =data[cpt_video_tag];
     			fseek(infos->hidden,nb_chunk*data_per_vtag, SEEK_SET);
     			uint32_t limit;
-    			if (cpt_video_tag==infos->host.file_info.flv.nb_video_tag-1){limit=data_per_vtag+reste;}
-    			else{cpt_video_tag=data_per_vtag;}
+    			if (cpt_video_tag==infos->host.file_info.flv.nb_video_tag-1){
+					limit=data_per_vtag+reste;
+					}
+    			else{
+					limit=data_per_vtag;
+					}
     			for(uint32_t i=0;i<limit;i++){
     				fread(&byte_cpy,sizeof(uint8_t),1,infos->hidden);
     				fwrite(&byte_cpy,sizeof(uint8_t),1,infos->res);
@@ -120,20 +124,23 @@ int insert_eoc(info_s * infos)
 
     			// /!\ taille à verif 
     			//modif previous tag size + ecriture
-    			if (cpt_video_tag==infos->host.file_info.flv.nb_video_tag-1){prev_tag_size+=reste+data_per_vtag;} //cas particulier dernier tag
-            	else {prev_tag_size+=reste+data_per_vtag;}	
+    			if (cpt_video_tag==infos->host.file_info.flv.nb_video_tag-1){
+					prev_tag_size += reste+data_per_vtag; //cas particulier dernier tag
+					} 
+            	else {
+					prev_tag_size += reste+data_per_vtag;
+					}	
     			prev_tag_size =htobe32(prev_tag_size);
     			fwrite(&prev_tag_size,sizeof(uint32_t),1,infos->res);
     			cpt_video_tag+=1;
-
-}
+			}
         }while(cpt_video_tag<infos->host.file_info.flv.nb_video_tag);
-
-
-}		  	
-
+	}
+	while(fread(&byte_cpy, sizeof(uint8_t), 1, infos->host.host))
+			fwrite(&byte_cpy,sizeof(uint8_t),1,infos->res);
+		
    
-    return 1;
+    return 0;
 }
 
 int extract_eoc(info_s * infos)
