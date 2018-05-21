b0d90f2b74c45683d97c4c413cd19b1c6db55240
Tristan <tristan.bessac@live.fr>
2018-05-21 04:14:21 +0200

    correction eoc sans mélange

--- src/lib/src/algo/eoc.c
+++ src/lib/src/algo/eoc.c
@@ -24,15 +24,16 @@ int insert_eoc(info_s * infos)
 	uint32_t prev_tag_size;
     uint32_t data_per_vtag = infos->hidden_length / infos->host.file_info.flv.nb_video_tag;
     uint32_t reste = infos->hidden_length % infos->host.file_info.flv.nb_video_tag;
-   
+  
    uint8_t *data;
     if (fseek(infos->host.host, 0, SEEK_SET) == -1)
         return perror("Can't do insertion EOC"), 1;
+    fseek(infos->hidden,0,SEEK_SET);
 	
 	/* Si on peut utiliser l'algorithme de protection des données :
 	 * initialisation de la protection */
     if(infos->host.file_info.flv.nb_video_tag<256){
-		printf("ICIIIII\n");
+		
     	data = malloc(infos->host.file_info.flv.nb_video_tag * sizeof(uint8_t));
         	if (!data)
             	return perror("Can't allocate memory Insertion"), 1;
@@ -44,9 +45,9 @@ int insert_eoc(info_s * infos)
          }
         protect_data(data,infos->host.file_info.flv.nb_video_tag,infos->passwd, infos->mode);
     }
-    else
-		if(data_per_vtag == 0)
-			cpt_video_tag = infos->host.file_info.flv.nb_video_tag;
+  //   else
+		// if(data_per_vtag == 0)
+		// 	cpt_video_tag = infos->host.file_info.flv.nb_video_tag;
 	
 	//recopie header
 	for(int j=0;j<13;j++){
@@ -61,6 +62,7 @@ int insert_eoc(info_s * infos)
 		fread(&tag_type, sizeof(uint8_t), 1, infos->host.host);
 		/* Si ce n'est pas un vidéo tag : recopie du chunk dans le fichier res */
 		if (tag_type!=9){
+			
 				fwrite(&tag_type, sizeof(uint8_t), 1, infos->res);
 				fread(&data_size, sizeof(data_size), 1, infos->host.host);
 				fwrite(&data_size, sizeof(uint32_t), 1, infos->res);
@@ -92,8 +94,10 @@ int insert_eoc(info_s * infos)
 			 * /!\ verifier que la taille à inscrire < 2^24
 			 * cas particulier dernier tag (ou du premier si on utilise pas l'algo de protection) */
 			if (cpt_video_tag==infos->host.file_info.flv.nb_video_tag-1 ||
-					((infos->host.file_info.flv.nb_video_tag >= 256) && (data_per_vtag == 0)))
-				data_size += reste+data_per_vtag;
+					((infos->host.file_info.flv.nb_video_tag >= 256) && (data_per_vtag == 0))){
+
+				
+				data_size += reste+data_per_vtag;}
 			else
 				data_size += data_per_vtag;
 			//recopie data size en big endian			
@@ -116,18 +120,21 @@ int insert_eoc(info_s * infos)
 			//écriture données caché
 			/* Si on utilise l'algo de mélange, on cherche le block de données à écrire. */
 			if(infos->host.file_info.flv.nb_video_tag<256){
+				
 				uint8_t nb_chunk = data[cpt_video_tag];
 				fseek(infos->hidden,nb_chunk*data_per_vtag, SEEK_SET);
 				limit = (cpt_video_tag == infos->host.file_info.flv.nb_video_tag-1) ? 
 						data_per_vtag + reste : data_per_vtag;
 			}
-			else
+			else{
+				
 				/* Si on utilise pas l'algo de protection : */
 				limit = ((data_per_vtag == 0) || (cpt_video_tag == infos->host.file_info.flv.nb_video_tag-1)) ? 
 						data_per_vtag+reste : data_per_vtag;
-			
+			}
 			/* Ecriture des données cachées */
 			for(uint32_t i=0;i<limit;i++){
+				
 				fread(&byte_cpy,sizeof(uint8_t),1,infos->hidden);
 				fwrite(&byte_cpy,sizeof(uint8_t),1,infos->res);
 			}
@@ -139,8 +146,9 @@ int insert_eoc(info_s * infos)
 			/*modification du previous tag size pour rajouter le nombre d'octets venant d'être écrits.
 			 * Et écriture de ce nombre */
 			if (cpt_video_tag==infos->host.file_info.flv.nb_video_tag-1 || 
-					((infos->host.file_info.flv.nb_video_tag >= 256) && (data_per_vtag == 0)))
+					((infos->host.file_info.flv.nb_video_tag >= 256) && (data_per_vtag == 0))){
 				prev_tag_size += reste+data_per_vtag; //cas particulier dernier tag
+				if ((infos->host.file_info.flv.nb_video_tag >= 256) && (data_per_vtag == 0))cpt_video_tag=infos->host.file_info.flv.nb_video_tag;}
 			else 
 				prev_tag_size +=data_per_vtag;
 				
@@ -172,7 +180,8 @@ int extract_eoc(info_s * infos)
 	uint32_t write_data;
 	uint32_t data_per_vtag = infos->hidden_length / infos->host.file_info.flv.nb_video_tag;
     uint32_t reste = infos->hidden_length % infos->host.file_info.flv.nb_video_tag;
-    uint8_t k=0;
+   
+    uint32_t k=0;
     uint8_t search_tag;
 	uint32_t cursor = 0;
    uint8_t *data;
@@ -193,9 +202,10 @@ int extract_eoc(info_s * infos)
 	}
 	else
 		if (data_per_vtag == 0)
-			k = infos->host.file_info.flv.nb_video_tag-1;
+			k = infos->host.file_info.flv.nb_video_tag-2;
 
 	do {
+		
 		/* On saute le header */
 		fseek(infos->host.host, 13, SEEK_SET);
 		cpt_video_tag = 0;
@@ -223,29 +233,32 @@ int extract_eoc(info_s * infos)
 		/* Si l'algo de protection des données n'a pas été utilisé */
 		else
 		{
-			cpt_video_tag = k;
+			cpt_video_tag = k+1;
 			search_tag = 1;
 			/* Recherche du prochain tag vidéo */
-			while(search_tag)
+			while(search_tag && fread(&tag_type, sizeof(uint8_t), 1, infos->host.host))
 			{
-				fread(&tag_type, sizeof(uint8_t), 1, infos->host.host);
-				if(tag_type != 9)
-				{
-					fread(&data_size, sizeof(uint32_t), 1, infos->host.host);
+								
+				fread(&data_size, sizeof(uint32_t), 1, infos->host.host);
 					//passage en 24 bits      
 					data_size = be32toh(data_size);
 					data_size >>= 8;
-					fseek(infos->host.host, data_size + 10, SEEK_CUR);
-				}
-				else
-					search_tag = 0;
+					
+					if (tag_type !=9)
+						fseek(infos->host.host, data_size + 10, SEEK_CUR);
+					else
+						search_tag = 0;
 			}
 		}
 
+		
 		/* Calcul le nombre d'octets à sauter pour arriver avant les données à récupérer */
-		if (cpt_video_tag==infos->host.file_info.flv.nb_video_tag){
+		if (cpt_video_tag==infos->host.file_info.flv.nb_video_tag-1){
+			
 			data_jump = data_size - reste - data_per_vtag + 6; //cas particulier dernier tag
 			write_data = reste + data_per_vtag;
+			
+
 		}
 		else {
 			data_jump = data_size - data_per_vtag;
@@ -254,6 +267,7 @@ int extract_eoc(info_s * infos)
 		fseek(infos->host.host, data_jump, SEEK_CUR);
 		/* Recopie des données dans le fichhier resultat */
 		for(uint32_t i = 0; i < write_data; i++){
+			
 			fread(&byte_cpy, sizeof(uint8_t), 1, infos->host.host);
 			fwrite(&byte_cpy,sizeof(uint8_t),1,infos->res);
 		} 
