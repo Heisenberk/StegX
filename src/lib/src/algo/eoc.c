b99f18841f7c45ef9cd678daa2c3d71d359d9149
uvsq21501810 <clement.caumes@ens.uvsq.fr>
2018-05-23 11:23:18 +0200

    Correction enleve printf et espaces

--- src/lib/src/algo/eoc.c
+++ src/lib/src/algo/eoc.c
@@ -30,17 +30,10 @@ int insert_eoc(info_s * infos)
     uint8_t *data;
     uint32_t *data2;
     int datab;
-    printf("dt %u\n",data_per_vtag);
-    printf("r %u\n",reste );
-    printf("nbt %u\n",infos->host.file_info.flv.nb_video_tag);
 
     if ((fseek(infos->host.host, 0, SEEK_SET) == -1) || (fseek(infos->hidden,0,SEEK_SET) == -1)){
         return perror("Can't do insertion EOC"), 1;}
-	
 
-
-	
-	 
     if(infos->host.file_info.flv.nb_video_tag<256){
 
     	data = malloc(infos->host.file_info.flv.nb_video_tag * sizeof(uint8_t));
@@ -50,31 +43,17 @@ int insert_eoc(info_s * infos)
 		
 		for(uint32_t i=0;i<infos->host.file_info.flv.nb_video_tag;i++){
 			data[i]=i;
-        	
-        	
  		}
  	protect_data(data,infos->host.file_info.flv.nb_video_tag,infos->passwd, STEGX_MODE_INSERT);
  	datab=0;
  	} 
  	else {
- 			printf("hey\n");
  			data2 = malloc(infos->host.file_info.flv.nb_video_tag * sizeof(uint32_t));
  			for(uint32_t i=0;i<infos->host.file_info.flv.nb_video_tag;i++){
 				data2[i]=i;
-				printf("data %u, i %u \n ",data2[i], i);
-
 			}
 			datab=1;
  		}
-
- 
-	 	//printf("%u\n",data[infos->host.file_info.flv.nb_video_tag+10]);
-	
-
- //~ for(uint8_t i=0;i<infos->host.file_info.flv.nb_video_tag;i++){
-            //~ printf("video_tag : %u  nb_blocks : %u\n",i, data[i]);
-            
-        //~ }
     	//recopie header
     	for(int j=0;j<13;j++){
     		if (fread(&byte_cpy, sizeof(uint8_t), 1, infos->host.host) != 1)
@@ -84,9 +63,7 @@ int insert_eoc(info_s * infos)
             
     	}
     	
-    	
     	do{
-
     		//recherche video tag
     		fread(&tag_type, sizeof(uint8_t), 1, infos->host.host);
     		if (tag_type!=9){
@@ -94,7 +71,7 @@ int insert_eoc(info_s * infos)
     		    	fread(&data_size, sizeof(data_size), 1, infos->host.host);
     		    	fwrite(&data_size, sizeof(uint32_t), 1, infos->res);
     		    	//passage en 24 bits      
-           	  	  	data_size = be32toh(data_size) >> 8;
+           	  	  	data_size = stegx_be32toh(data_size) >> 8;
         			//recopie data + 6 octets
         			for(uint32_t j=0;j<data_size+6;j++){
         				fread(&byte_cpy, sizeof(uint8_t), 1, infos->host.host);
@@ -112,7 +89,7 @@ int insert_eoc(info_s * infos)
     			/* /!\ */
     			//l'octet de droite qui fait parti des datas
     			//passage en 24 bits      
-            	data_size= be32toh(data_size); 
+            	data_size= stegx_be32toh(data_size); 
             	uint8_t tmp = data_size;
             	data_size>>= 8;
 
@@ -160,17 +137,11 @@ int insert_eoc(info_s * infos)
 
     			uint32_t nb_block=(infos->host.file_info.flv.nb_video_tag<256) ?
     									data[cpt_video_tag]:data2[cpt_video_tag];
-    			
-    			
-    		// 	printf(" nb tag%u\n",infos->host.file_info.flv.nb_video_tag);
 
-    	 /*ici*/		printf("nb_block %u\n", nb_block);
-    			printf("cpt %u\n",cpt_video_tag);
     			fseek(infos->hidden, nb_block*data_per_vtag, SEEK_SET);
     			uint32_t limit;
 				limit = (nb_block == infos->host.file_info.flv.nb_video_tag-1) ? 
 										data_per_vtag + reste : data_per_vtag;
-				printf("limit %u\n",limit );
     			for(uint32_t i=0;i<limit;i++){
     				fread(&byte_cpy,sizeof(uint8_t),1,infos->hidden);
     				fwrite(&byte_cpy,sizeof(uint8_t),1,infos->res);
@@ -178,7 +149,7 @@ int insert_eoc(info_s * infos)
     			}
     			 //lecture previous tagsize
     			fread(&prev_tag_size,sizeof(uint32_t),1,infos->host.host);
-    			prev_tag_size = be32toh(prev_tag_size);
+    			prev_tag_size = stegx_be32toh(prev_tag_size);
 
     			// /!\ taille à verif 
     			//modif previous tag size + ecriture
@@ -186,7 +157,7 @@ int insert_eoc(info_s * infos)
 					prev_tag_size += reste + data_per_vtag; //cas particulier dernier tag
             	else 
 					prev_tag_size += data_per_vtag;
-    			prev_tag_size = htobe32(prev_tag_size);
+    			prev_tag_size = stegx_htobe32(prev_tag_size);
     			fwrite(&prev_tag_size,sizeof(uint32_t),1,infos->res);
     			cpt_video_tag += 1;
 			}
@@ -238,44 +209,35 @@ int extract_eoc(info_s * infos)
 		
 		for(uint32_t i=0;i<infos->host.file_info.flv.nb_video_tag;i++){
 			data[i]=i;
-        	
-        	printf("data %u, i %u \n ",data[i], i);
  		}
  	protect_data(data,infos->host.file_info.flv.nb_video_tag,infos->passwd, STEGX_MODE_INSERT);
  	datab=0;
  	} 
  	else {
- 			printf("hey\n");
  			data2 = malloc(infos->host.file_info.flv.nb_video_tag * sizeof(uint32_t));
  			for(uint32_t i=0;i<infos->host.file_info.flv.nb_video_tag;i++){
 				data2[i]=i;
-				printf("data %u, i %u \n ",data2[i], i);
-
 			}
 			datab=1;
  		}
 	
- 
 		do {
-
 				//saute de header
 				fseek(infos->host.host, 13, SEEK_SET);
 				cursor = 0;
 				
 				if(datab){
-					printf("cursor %u data %u  nb_block %u\n",cursor,data2[cursor], nb_block);
 					while(data2[cursor] != nb_block)
 						cursor++;
 					cpt_video_tag = -1;
 					/* Recherche du tag vidéo numéro cursor */
-					printf("%u\n",cursor);
 					while(cursor != cpt_video_tag){
 						fread(&tag_type, sizeof(uint8_t), 1, infos->host.host);
 						if(tag_type == 9)
 							cpt_video_tag++;
 						fread(&data_size, sizeof(uint32_t), 1, infos->host.host);
 						//passage en 24 bits      
-						data_size = be32toh(data_size) >> 8;
+						data_size = stegx_be32toh(data_size) >> 8;
 						/* Si c'est pas le tag recherché, on jump 
 						 * les 6 octets après data_size + les data + le previous tag size (4 octets) */ 
 						if(cursor != cpt_video_tag)
@@ -294,18 +256,12 @@ int extract_eoc(info_s * infos)
 							cpt_video_tag++;
 						fread(&data_size, sizeof(uint32_t), 1, infos->host.host);
 						//passage en 24 bits      
-						data_size = be32toh(data_size) >> 8;
+						data_size = stegx_be32toh(data_size) >> 8;
 						/* Si c'est pas le tag recherché, on jump 
 						 * les 6 octets après data_size + les data + le previous tag size (4 octets) */ 
 						if(cursor != cpt_video_tag)
 							fseek(infos->host.host, data_size + 10, SEEK_CUR);
 					}
-
-
-
-
-
-
 				}
 			/* Calcul le nombre d'octets à sauter pour arriver avant les données à récupérer */
 			if (nb_block == infos->host.file_info.flv.nb_video_tag - 1){
