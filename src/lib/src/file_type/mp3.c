2f712611c57863ced0759ab9e97dab2ca46f1874
Pierre Ayoub <drac78@live.fr>
2018-05-27 13:33:01 +0200

    dev-audio:mp3: Fonctions de parsing
    Fonctions de base pour pouvoir parser et analyser un fichier MP3. Utile
    pour se déplacer correctement dans le fichier et remplir les
    informations dans fill_host_info.

--- src/lib/src/file_type/mp3.c
+++ src/lib/src/file_type/mp3.c
@@ -15,12 +15,151 @@
 #include "stegx_errors.h"
 #include "mp3.h"
 
+/** Signature du MPEG 1 Layer III. */
+#define SIG_MPEG1_LAYER3 0xFFFA0000
+/** Signature du MPEG 2 Layer III. */
+#define SIG_MPEG2_LAYER3 0xFFF20000
+/** Signature de l'ID3v1. */
+#define SIG_ID3V1 0x54414700
+/** Signature de l'ID3v2. */
+#define SIG_ID3V2 0x49443300
+
+/** Masque à appliquer pour reconnaître la signature du MPEG 1/2 Layer III. */
+#define MASK_MPEG_LAYER3 0xFFFE0000
+/** Masque à appliquer pour reconnaître la signature de l'ID3. */
+#define MASK_ID3 0xFFFFFF00
+
+/** Taille d'un TAG ID3v1. */
+#define TAG_ID3V1_SIZE 128
+/** Taille d'un header de tag ID3v2. */
+#define TAG_ID3V2_HEADER_SIZE 10
+
+// TODO : Rajouter doc.
+
+static int mp3_id3v2_hdr_test(const uint32_t hdr)
+{
+    return (hdr & MASK_ID3) == SIG_ID3V2;
+}
+
+static uint32_t mp3_id3v2_size_unsyncsafe(const uint32_t s)
+{
+    assert(!(0x80808080 & s) && "L'entier doit être un entier syncsafe");
+    return (0x0000007F & s) | ((0x00007F00 & s) >> 1) | ((0x007F0000 & s) >> 2) | ((0x7F000000 & s) >> 3);
+}
+
+static int mp3_id3v2_hdr_is_unsync(const uint8_t flags)
+{
+    assert(!(0x0F & flags) && "L'entier doit être un flag ID3v2");
+    return (0x80 & flags) >> 7;
+}
+
+static int mp3_id3v2_hdr_get_version(const uint32_t hdr)
+{
+    assert(mp3_id3v2_hdr_test(hdr) && "Le header doit être un header ID3v2");
+    return 0x000000FF & hdr;
+}
+
+static int mp3_id3v2_tag_seek(FILE * f)
+{
+    assert(f && ftell(f) == 4 && "Les 4 premiers octets du fichier (header ID3v2) doivent êtres lus");
+    uint8_t flags = 0;
+    uint32_t size = 0;
+    /* Lecture des flags et de la taille. */
+    if (fseek(f, 1, SEEK_CUR) || fread(&flags, sizeof(flags), 1, f) != 1)
+        return perror("mp3_id3v2_tag_seek: Can't read flags of the header"), -1;
+    if (fread(&size, sizeof(size), 1, f) != 1)
+        return perror("mp3_id3v2_tag_seek: Can't read size of the header"), -1;
+    assert(ftell(f) == 10);
+    return fseek(f, mp3_id3v2_size_unsyncsafe(stegx_be32toh(size)) + TAG_ID3V2_HEADER_SIZE, SEEK_SET);
+}
+
+static int mp3_mpeg_hdr_get_version(const uint32_t hdr)
+{
+    assert(mp3_mpeg_hdr_test(hdr) && "Le header doit être un header MPEG 1/2 Layer III");
+    static const int ver[4] = {0, 0, 2, 1};
+    return ver[(hdr & 0x180000) >> 19];
+}
+
+static int mp3_mpeg_hdr_is_protec(const uint32_t hdr)
+{
+    assert(mp3_mpeg_hdr_test(hdr) && "Le header doit être un header MPEG 1/2 Layer III");
+    return !((hdr & 0x010000) >> 16);
+}
+
+static int mp3_mpeg_hdr_is_padding(const uint32_t hdr)
+{
+    assert(mp3_mpeg_hdr_test(hdr) && "Le header doit être un header MPEG 1/2 Layer III");
+    return (hdr & 0x00000200) >> 9;
+}
+
+static int mp3_mpeg_hdr_get_samprate(const uint32_t hdr)
+{
+    assert(mp3_mpeg_hdr_test(hdr) && "Le header doit être un header MPEG 1/2 Layer III");
+    static const int samp[3][4] = {{0}, {44100, 48000, 32000, 0}, {22050, 24000, 16000, 0}};
+    return samp[mp3_mpeg_hdr_get_version(hdr)][(hdr & 0x00000C00) >> 10];
+}
+
+static int mp3_mpeg_hdr_get_bitrate(const uint32_t hdr)
+{
+    assert(mp3_mpeg_hdr_test(hdr) && "Le header doit être un header MPEG 1/2 Layer III");
+    static const int bit[3][16] = {{0},
+        {0, 32, 40, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 0},
+        {0, 8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160, 0}};
+    return bit[mp3_mpeg_hdr_get_version(hdr)][(hdr & 0x0000F000) >> 12];
+}
+
+static int mp3_mpeg_hdr_get_size(const uint32_t hdr)
+{
+    assert(mp3_mpeg_hdr_test(hdr) && "Le header doit être un header MPEG 1/2 Layer III");
+    return ((144000 * mp3_mpeg_hdr_get_bitrate(hdr)) / mp3_mpeg_hdr_get_samprate(hdr)) + mp3_mpeg_hdr_is_padding(hdr);
+}
+
+int mp3_mpeg_hdr_test(const uint32_t hdr)
+{
+    return (hdr & MASK_MPEG_LAYER3) == SIG_MPEG1_LAYER3 || (hdr & MASK_MPEG_LAYER3) == SIG_MPEG2_LAYER3;
+}
+
+int mp3_mpeg_fr_seek(const uint32_t hdr, FILE * f)
+{
+    assert(mp3_mpeg_hdr_test(hdr) && "Le header doit être un header MPEG 1/2 Layer III");
+    assert(f);
+    return fseek(f, mp3_mpeg_hdr_get_size(hdr) - sizeof(hdr), SEEK_CUR);
+}
+
+long int mp3_mpeg_fr_find_first(FILE * f)
+{
+    assert(f);
+    uint32_t hdr = 0;
+    if (fseek(f, 0, SEEK_SET) || fread(&hdr, sizeof(hdr), 1, f) != 1)
+        return perror("mp3_mpeg_fr_find_first: Can't read MP3 first signature"), -1;
+    hdr = stegx_be32toh(hdr);
+    /* Si on est sur de l'ID3v2, on saute le tag. */
+    if (mp3_id3v2_hdr_test(hdr) && mp3_id3v2_tag_seek(f))
+        return perror("mp3_mpeg_fr_find_first: Can't skip over ID3v2 tag"), -1;
+    /* Si on était sur une frame MPEG 1/2 Layer III ou que l'on est dessus après avoir sauté le tag ID3v2. */
+    return mp3_mpeg_hdr_test(hdr) || ((fread(&hdr, sizeof(hdr), 1, f) == 1) && mp3_mpeg_hdr_test(stegx_be32toh(hdr))) ?
+        ftell(f) - (int)sizeof(hdr) : -1;
+}
+
+int mp3_id3v1_hdr_test(const uint32_t hdr)
+{
+    assert(mp3_mpeg_hdr_test(hdr) && "Le header doit être un header MPEG 1/2 Layer III");
+    return (hdr & MASK_ID3) == SIG_ID3V1;
+}
+
+int mp3_id3v1_tag_seek(FILE * f)
+{
+    assert(f);
+    return fseek(f, TAG_ID3V1_SIZE - sizeof(uint32_t), SEEK_CUR);
+}
+
 type_e stegx_test_file_mp3(FILE * file)
 {
     assert(file);
     uint32_t sig = 0;
-    if (fseek(file, 0, SEEK_SET) || (fread(&sig, sizeof(uint32_t), 1, file) != 1))
-        return perror("MP3 test file: Can't read MP3 signature"), -1;
-    return (sig & MASK_MP3_MPEGX_LAYER3) == SIG_MP3_MPEG1_LAYER3 || (sig & MASK_MP3_MPEGX_LAYER3) == SIG_MP3_MPEG2_LAYER3
-        || (sig & MASK_MP3_ID3V2) == SIG_MP3_ID3V2 ? MP3 : UNKNOWN;
+    if (fseek(file, 0, SEEK_SET) || (fread(&sig, sizeof(sig), 1, file) != 1))
+        return perror("stegx_test_file_mp3: Can't read first 4 bytes"), -1;
+    sig = stegx_be32toh(sig);
+    return mp3_id3v2_hdr_test(sig) || mp3_mpeg_hdr_test(sig) ? MP3 : UNKNOWN;
 }
+
--- src/lib/src/file_type/mp3.h
+++ src/lib/src/file_type/mp3.h
@@ -13,33 +13,28 @@
 
 #include "common.h"
 
-/** Signature du MP3 MPEG 1 Layer III (sans CRC, contenant ou pas de l'ID3v1 à la fin). */
-#define SIG_MP3_MPEG1_LAYER3 0xFAFF
-/** Signature du MP3 MPEG 2 Layer III (sans CRC, contenant ou pas de l'ID3v1 à la fin). */
-#define SIG_MP3_MPEG2_LAYER3 0xF2FF
-/** Signature du MP3 avec tag ID3v2. */
-#define SIG_MP3_ID3V2 0x00334449
-
-/** Masque à appliquer à un entier de 32 bits pour reconnaître la
- * signature d'un MP3 MPEG 1/2 Layer III. */
-#define MASK_MP3_MPEGX_LAYER3 0x0000FEFF
-/** Masque à appliquer à un entier de 32 bits pour reconnaître la
- * signature d'un MP3 avec tag ID3v2. */
-#define MASK_MP3_ID3V2 0x00FFFFFF
-
 /**
  * @brief Structure du format MP3.
  */
 struct mp3 {
+    long int fr_frst_adr; /*!< Adresse du header de la première frame MPEG 1/2 Layer III. */
+    long int fr_nb;       /*!< Nombre de frame MPEG 1/2 Layer III. */
+    long int eof;         /*!< Adresse de la fin du fichier officiel (sans signature et données cachées). */
 };
 
 /** Type du format MP3. */
 typedef struct mp3 mp3_s;
 
+int mp3_mpeg_hdr_test(uint32_t hdr);
+int mp3_mpeg_fr_seek(uint32_t hdr, FILE * f);
+long int mp3_mpeg_fr_find_first(FILE * f);
+int mp3_id3v1_hdr_test(uint32_t hdr);
+int mp3_id3v1_tag_seek(FILE * f);
+
 /**
- * @brief Test si le fichier est un fichier WAVE.
+ * @brief Test si le fichier est un fichier MP3.
  * @param file Fichier à tester.
- * @req Le pointeur ne doit pas être null et le fichier ouvert en lecture.
+ * @req Le fichier doit être ouvert en lecture.
  * @return \r{MP3}, \r{UNKNOWN} ou -1 en cas d'erreur. 
  */
 type_e stegx_test_file_mp3(FILE * file);
